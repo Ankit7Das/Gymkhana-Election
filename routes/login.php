504359404b07996615305469443ba29cd751eac7
debarghya472 <debarghya_1901me19@iitp.ac.in>
2020-12-03 22:53:03 +0530

    added imap for user login

--- routes/login.php
+++ routes/login.php
@@ -2,41 +2,23 @@
 
 $webmail = trim(htmlspecialchars($_POST["webid"]));
 $password = trim(htmlspecialchars($_POST["pass"]));
-$data = [
-  'email' => $webmail,
-  'password'=>$password
-];
-$httpcode= httpPost("http://localhost:4000/api/students/login",$data);// use LDAP url here
+$mailhost="172.16.1.224:995/pop3/ssl/novalidate-cert";
+
+$username =substr($webmail, 0,strpos($webmail, "@"));
+$pop = imap_open('{' .$mailhost. '}', $username, $password);
 
 
-if($httpcode == 200)
+if ($pop == false) 
 {
-    echo "success!!!";
-    session_start();
-    $_SESSION['webmail'] = $webmail;
-    echo "<script> location.href='../public/User_details_new.html'; </script>";
-}else{
-    echo "Invalid Credentials";
-}
-//using php curl (sudo apt-get install php-curl) 
-function httpPost($url, $data){
-  $curl = curl_init($url);
-// Set the CURLOPT_RETURNTRANSFER option to true
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-// Set the CURLOPT_POST option to true for POST request
-curl_setopt($curl, CURLOPT_POST, true);
-// Set the request data as JSON using json_encode function
-curl_setopt($curl, CURLOPT_POSTFIELDS,  json_encode($data));
-// Set custom headers for RapidAPI Auth and Content-Type header
-curl_setopt($curl, CURLOPT_HTTPHEADER, [
-  'Content-Type: application/json'
-]);
-  
-  $response = curl_exec($curl);
-  $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-  curl_close($curl);
-
-  return $httpcode;
+    echo "<center>Authentication Failed</center>\n";
+    echo "<script> location.href='../public/error_signup.html'; </script>";
+} 
+else 
+{
+      session_start();
+      imap_close($pop);
+      $_SESSION['webmail'] = $webmail;
+      echo "<script> location.href='../public/User_details_new.html'; </script>";
 }
 
 ?>
