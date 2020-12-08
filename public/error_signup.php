ec11dc59133dbe73159fda4c887aa9f052b467ca
debarghya472 <debarghya_1901me19@iitp.ac.in>
2020-12-09 00:00:53 +0530

    minor checks

---
+++ public/error_signup.php
@@ -0,0 +1,21 @@
+
+ <!DOCTYPE html>
+ <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" >
+     <head>
+         <title>
+             Error occured 
+         </title>
+     </head>
+     <body>
+     <diV style="text-align: center;">
+         <h3>
+             Please enter the correct detials
+         </h3>
+     </diV>
+         <?php
+           sleep(3);
+           echo "<script> location.href='../public/User_details_new.html'; </script>";
+           ?>
+           
+     </body>
+ </html>
