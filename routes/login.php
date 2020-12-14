1d52f1aa3193eefd31cda627b68ea975776c289c
debarghya472 <debarghya_1901me19@iitp.ac.in>
2020-12-14 11:16:57 +0530

    added error_login

--- routes/login.php
+++ routes/login.php
@@ -5,13 +5,12 @@ $password = trim(htmlspecialchars($_POST["pass"]));
 $mailhost="stud.iitp.ac.in:995/pop3/ssl/novalidate-cert";
 
 $username =substr($webmail, 0,strpos($webmail, "@"));
-// $pop = imap_open('{' .$mailhost. '}', $username, $password);
-$pop =true;
+$pop = imap_open('{' .$mailhost. '}', $username, $password);
 
 if ($pop == false) 
 {
     echo "<center>Authentication Failed</center>\n";
-    echo "<script> location.href='../public/error.html'; </script>";
+    echo "<script> location.href='../public/error_login.html'; </script>";
 } 
 else 
 {
